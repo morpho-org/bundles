1096f947e71cf36c5d2e64370c5c414e01884d8b
Quentin Garchery <garchery.quentin@gmail.com>
2026-06-11 14:42:19 +0200

    move imports

---
+++ test/imports/MidnightImport.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2026 Morpho Association
+pragma solidity 0.8.34;
+// Force foundry to compile Midnight without importing it in the tests.
+
+import {Midnight} from "../../lib/midnight/src/Midnight.sol";
