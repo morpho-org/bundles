c0270401c7eb0a76f477420694f74364728537b5
Quentin Garchery <garchery.quentin@gmail.com>
2026-07-13 20:38:51 +0200

    Update src/vault-force-withdraw/VaultForceWithdrawBundlesV1.sol
    Co-authored-by: MathisGD <74971347+MathisGD@users.noreply.github.com>
    Signed-off-by: Quentin Garchery <garchery.quentin@gmail.com>

--- src/vault-force-withdraw/VaultForceWithdrawBundlesV1.sol
+++ src/vault-force-withdraw/VaultForceWithdrawBundlesV1.sol
@@ -106,7 +106,6 @@ contract VaultForceWithdrawBundlesV1 is IVaultForceWithdrawBundlesV1, IMorphoSup
         require(IMorphoMarketV1AdapterV2(adapter).morpho() == BLUE, MorphoMismatch());
 
         permitShares(vault, sharesPermit);
-
         TokenLib.forceApproveMax(marketParams[0].loanToken, BLUE);
 
         uint256 penalty = IVaultV2(vault).forceDeallocatePenalty(adapter);
